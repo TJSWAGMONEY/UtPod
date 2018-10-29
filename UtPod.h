8a84cae6547b1644c307f6bf36865266ab54f7c5
JamesLin1996 <32826962+JamesLin1996@users.noreply.github.com>
2018-10-29 05:52:35 -0500

    added 2 essential private methods
    -added getMemoryUsed();
    -added getNumSongs();
    -Priebe said its only ok to add private methods into UtPod
        > https://piazza.com/class/jiz4qoj5drl2mb?cid=370

--- UtPod.h
+++ UtPod.h
@@ -2,144 +2,130 @@
 
 #ifndef UTPOD_H
 #define UTPOD_H
-#include "Song.h"
+#include "song.h"
 
 //UtPod class declaration
 class UtPod
 {
-   private:
-      static const int MAX_MEMORY = 512;
-      static const int SUCCESS = 0;
-      static const int NO_MEMORY = -1;
-      static const int NOT_FOUND = -2;
-      
-      struct SongNode
-      {
-         Song s;
-         SongNode *next;
-      };
-      
-      SongNode *songs;  //the head pointer
-      
-      int memSize;
-   
-   public:
-      //Default constructor
-      //set the memory size to MAX_MEMORY
-      UtPod();
+private:
+    static const int MAX_MEMORY = 512;
+    static const int SUCCESS = 0;
+    static const int NO_MEMORY = -1;
+    static const int NOT_FOUND = -2;
 
-      //Constructor with size parameter
-      //The user of the class will pass in a size.
-      //If the size is greater than MAX_MEMORY or less than or equal to 0,
-      //set the size to MAX_MEMORY.
-      UtPod(int size);
+    struct SongNode
+    {
+        Song s;
+        SongNode *next;
+    };
 
-      /* FUNCTION - int addSong
-       * attempts to add a new song to the UtPod
-           o returns a 0 if successful
-           o returns -1 if not enough memory to add the song
+    SongNode *songs;  //the head pointer
 
-       precondition: s is a valid Song
+    int memSize;
 
+    //private method used to find total memory used in UtPod
+    int getMemoryUsed();
+
+    //private method used to find number of songs in UtPod
+    int getNumSongs();
+
+public:
+    //Default constructor
+    //set the memory size to MAX_MEMORY
+    UtPod();
+
+    //Constructor with size parameter
+    //The user of the class will pass in a size.
+    //If the size is greater than MAX_MEMORY or less than or equal to 0,
+    //set the size to MAX_MEMORY.
+    UtPod(int size);
+
+    /* FUNCTION - int addSong
+     * attempts to add a new song to the UtPod
+         o returns a 0 if successful
+         o returns -1 if not enough memory to add the song
+     precondition: s is a valid Song
+     input parms -
+     output parms -
+    */
+
+    int addSong(Song const &s);
+
+
+    /* FUNCTION - int removeSong
+     * attempts to remove a song from the UtPod
+     * removes the first instance of a song that is in the the UtPod multiple times
+         o returns 0 if successful
+         o returns -1 if nothing is removed
        input parms -
-
        output parms -
-      */
+    */
 
-      int addSong(Song const &s);
-
-
-      /* FUNCTION - int removeSong
-       * attempts to remove a song from the UtPod
-       * removes the first instance of a song that is in the the UtPod multiple times
-           o returns 0 if successful
-           o returns -1 if nothing is removed
+    int removeSong(Song const &s);
 
 
-         input parms -
+    /* FUNCTION - void shuffle
+     *  shuffles the songs into random order
+        o will do nothing if there are less than two songs in the current list
+       input parms -
+       output parms -
+    */
 
-         output parms -
-      */
-
-      int removeSong(Song const &s);
-
-
-      /* FUNCTION - void shuffle
-       *  shuffles the songs into random order
-          o will do nothing if there are less than two songs in the current list
-
-         input parms -
-
-         output parms -
-      */
-
-      void shuffle();
+    void shuffle();
 
 
-      /* FUNCTION - void showSongList
-       * prints the current list of songs in order from first to last to standard output
-       * format - Title, Artist, size in MB (one song per line)
+    /* FUNCTION - void showSongList
+     * prints the current list of songs in order from first to last to standard output
+     * format - Title, Artist, size in MB (one song per line)
+       input parms -
+       output parms -
+    */
 
-         input parms -
-
-         output parms -
-      */
-
-      void showSongList();
+    void showSongList();
 
 
-      /* FUNCTION - void sortSongList
-       *  sorts the songs in ascending order
-          o will do nothing if there are less than two songs in the current list
+    /* FUNCTION - void sortSongList
+     *  sorts the songs in ascending order
+        o will do nothing if there are less than two songs in the current list
+       input parms -
+       output parms -
+    */
 
-         input parms -
-
-         output parms -
-      */
-
-      void sortSongList();
+    void sortSongList();
 
 
-      /* FUNCTION - void clearMemory
-       * clears all the songs from memory
-
-         input parms -
-
-         output parms -
-      */
-      void clearMemory();
+    /* FUNCTION - void clearMemory
+     * clears all the songs from memory
+       input parms -
+       output parms -
+    */
+    void clearMemory();
 
 
-      /* FUNCTION - int getTotalMemory
-       *  returns the total amount of memory in the UtPod
-          o will do nothing if there are less than two songs in the current list
+    /* FUNCTION - int getTotalMemory
+     *  returns the total amount of memory in the UtPod
+        o will do nothing if there are less than two songs in the current list
+       input parms -
+       output parms -
+    */
 
-         input parms -
-
-         output parms -
-      */
-
-      int getTotalMemory() {
-         return memSize;
-      }
+    int getTotalMemory() {
+        return memSize;
+    }
 
 
 
-      /* FUNCTION - int getRemainingMemory
-       *  returns the amount of memory available for adding new songs
+    /* FUNCTION - int getRemainingMemory
+     *  returns the amount of memory available for adding new songs
+       input parms -
+       output parms -
+    */
 
-         input parms -
-
-         output parms -
-      */
-
-      int getRemainingMemory();
+    int getRemainingMemory();
 
 
-      ~UtPod();
- 
+    ~UtPod();
+
 };
-
-
 
 #endif
